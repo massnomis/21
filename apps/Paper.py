aa754ffc9063605fd0c420dc360df7d72b3d6c8c
massnomis <massnomis@gmail.com>
2022-06-11 15:54:55 -0400

    Update Paper.py

--- apps/Paper.py
+++ apps/Paper.py
@@ -1,4 +1,3 @@
-
 from itertools import accumulate
 import pandas as pd
 import streamlit as st
@@ -9,9 +8,12 @@ def app():
     page = st.container()
 
     tftkn = pd.read_json('https://node-api.flipsidecrypto.com/api/v2/queries/9e36dbae-40e6-4443-a54a-b7166e78ed71/data/latest')
+    page.write("Commercial Paper: Within decentralized Finance, a look into TruFi")
     page.markdown('https://app.truefi.io/lend')
     llal = px.line(tftkn,x='DAYZ',y='PRICE', color = 'TFTOKEN',render_mode="SVG")
     page.plotly_chart(llal)
-    page.write(tftkn)
+    page.write("Accumulated Yeild on Selected Paper Pools")
+    # page.write(tftkn)
     mmm = px.line(tftkn,x='DAYZ',y='APY', color = 'TFTOKEN',render_mode="SVG")
     page.plotly_chart(mmm)
+    page.write("Annualized Yeilds on TruFi Pools")
