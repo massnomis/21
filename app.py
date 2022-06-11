30ae3a449a791eebf954632726be83cca6b4fc32
massnomis <massnomis@gmail.com>
2022-06-11 15:55:37 -0400

    Update app.py

--- app.py
+++ app.py
@@ -10,20 +10,12 @@ st.set_page_config(layout="wide")
 
 app.add_app("home", home.app)
 
-app.add_app("Paper", Paper.app)
-app.add_app("Staked_Deriv", Staked_Deriv.app)
-app.add_app("stables", stables.app)
-app.add_app("stables_funding", stables_funding.app)
-app.add_app("stables_spot_funding", stables_spot_funding.app)
-app.add_app("stables_spot_funding2", stables_spot_funding2.app)
+app.add_app("Commercial Paper", Paper.app)
+app.add_app("Staking and Staked Derivatives", Staked_Deriv.app)
+app.add_app("Stablecoins, price variation and opportunities", stables.app)
+app.add_app("Underlying interest rates of stablecoins, and of their derivatives", stables_funding.app)
+app.add_app("Stables_spot_funding", stables_spot_funding.app)
+app.add_app("Stables_spot_funding2", stables_spot_funding2.app)
 
 app.add_app("CVXCRV_prices", CVXCRV_prices.app)
-
-
-
-
-
-
-
-
 app.run()
