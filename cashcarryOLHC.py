b939654259e2fb3ddfe184b3baf2d377be5274ab
vladiguez <ylannguez@gmail.com>
2022-06-21 06:54:46 +0000

    giug

--- cashcarryOLHC.py
+++ cashcarryOLHC.py
@@ -23,15 +23,15 @@ st.set_page_config(layout="wide")
 
 
 
-st.text('Fixed width text')
-st.markdown('_Markdown_') # see *
-st.latex(r''' e^{i\pi} + 1 = 0 ''')
-st.write('Most objects') # df, err, func, keras!
-st.write(['st', 'is <', 3]) # see *
-st.title('My title')
-st.header('My header')
-st.subheader('My sub')
-st.code('for i in range(8): foo()')
+# st.text('Fixed width text')
+# st.markdown('_Markdown_') # see *
+# st.latex(r''' e^{i\pi} + 1 = 0 ''')
+# st.write('Most objects') # df, err, func, keras!
+# st.write(['st', 'is <', 3]) # see *
+# st.title('My title')
+# st.header('My header')
+# st.subheader('My sub')
+# st.code('for i in range(8): foo()')
 
 
 
