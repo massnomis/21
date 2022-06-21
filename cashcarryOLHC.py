1a6077e8de23e891e46d3ffee75c4af0e0fa5c7f
vladiguez <ylannguez@gmail.com>
2022-06-21 06:40:25 +0000

    test

--- cashcarryOLHC.py
+++ cashcarryOLHC.py
@@ -20,6 +20,21 @@ import requests
 import pandas as pd
 import random
 st.set_page_config(layout="wide")
+
+
+
+st.text('Fixed width text')
+st.markdown('_Markdown_') # see *
+st.latex(r''' e^{i\pi} + 1 = 0 ''')
+st.write('Most objects') # df, err, func, keras!
+st.write(['st', 'is <', 3]) # see *
+st.title('My title')
+st.header('My header')
+st.subheader('My sub')
+st.code('for i in range(8): foo()')
+
+
+
 # premiums = requests.get('https://ftxpremiums.com/assets/data/premiums.json')
 # premiums = json.loads(premiums.text)
 # premiums = pd.DataFrame(premiums)
