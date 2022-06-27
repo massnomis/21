9afc510dce9e724a305ef41a3276fc154e52dfd0
massnomis <massnomis@gmail.com>
2022-06-27 10:36:53 +0000

    asdakj

--- torns.py
+++ torns.py
@@ -98,7 +98,8 @@ async def another_name():
                         asks.loc[asks["price_ask"] == asks_update.loc[i]["price_ask"], "size_ask"] = asks_update.loc[i]["size_ask"]
                         asks.dropna(inplace=True)
 
-                        asks = asks.append(asks_update, ignore_index=True)
+                        asks = pd.concat([asks, pd.DataFrame.from_records(asks_update)])
+
                         asks = asks.drop_duplicates(subset=['price_ask'], keep='first')
                         asks.sort_values(by=['price_ask'], inplace=True)
                         asks.reset_index(drop = True, inplace=False)
@@ -116,7 +117,8 @@ async def another_name():
                         # bids.append(bids_update.loc[i])
                         bids.loc[bids["price_bid"] == bids_update.loc[i]["price_bid"], "size_bid"] = bids_update.loc[i]["size_bid"]
                         bids.dropna(inplace=True)
-                        bids = bids.append(bids_update, ignore_index=True)
+                        bids = pd.concat([bids, pd.DataFrame.from_records(bids_update)])
+
                         bids = bids.drop_duplicates(subset=['price_bid'], keep='first')
                         bids.sort_values(by=['price_bid'], inplace=True, ascending=False)
                         bids.reset_index(drop=True, inplace=False)
@@ -135,22 +137,22 @@ async def another_name():
                     bids['accumulated_price']  = (bids['price_bid']) * bids['size_bid']
                     bids['accumulated_avg_price'] = (list(accumulate(bids['accumulated_price'])))  / bids['accumulated']
                     bids['cash_equivelant'] = bids['accumulated'] * bids['accumulated_avg_price']                
-                    for i in range(1, 2):
-                        cols = st.columns(2)
-                        cols[0].subheader("bids")
+                    # for i in range(1, 2):
+                    #     cols = st.columns(2)
+                    #     cols[0].subheader("bids")
 
-                        cols[0].write(bids)
-                        cols[1].subheader("asks")
+                    #     cols[0].write(bids)
+                    #     cols[1].subheader("asks")
 
-                        cols[1].write(asks)
-                    # st.write(asks,bids, use_container_width=True)
+                    #     cols[1].write(asks)
+                    # # st.write(asks,bids, use_container_width=True)
                     # st.write(asks_update,bids_update, use_container_width=True)
                     
-                    fig = make_subplots(specs=[[{"secondary_y": True}]])
-                    fig.add_trace(go.Scatter(x=asks['price_ask'], y=asks['accumulated'], name="asks"),secondary_y=True,)
-                    fig.add_trace(go.Scatter(x=bids['price_bid'], y=bids['accumulated'], name="bids"),secondary_y=True,)
-                    fig.update_layout(title_text="orderbook")
-                    st.plotly_chart(fig, use_container_width=True)
+                    # fig = make_subplots(specs=[[{"secondary_y": True}]])
+                    # fig.add_trace(go.Scatter(x=asks['price_ask'], y=asks['accumulated'], name="asks"),secondary_y=True,)
+                    # fig.add_trace(go.Scatter(x=bids['price_bid'], y=bids['accumulated'], name="bids"),secondary_y=True,)
+                    # fig.update_layout(title_text="orderbook")
+                    # st.plotly_chart(fig, use_container_width=True)
 
                     fig = make_subplots(specs=[[{"secondary_y": True}]])
                     fig.add_trace(go.Bar(x=asks['price_ask'], y=asks['size_ask'], name="asks"),secondary_y=True,)
@@ -158,11 +160,11 @@ async def another_name():
                     fig.update_layout(title_text="orderbook")
                     st.plotly_chart(fig, use_container_width=True)
 
-                    fig = make_subplots(specs=[[{"secondary_y": True}]])
-                    fig.add_trace(go.Scatter(x=asks['accumulated_avg_price'], y=asks['cash_equivelant'], name="asks"),secondary_y=True,)
-                    fig.add_trace(go.Scatter(x=bids['accumulated_avg_price'], y=bids['cash_equivelant'], name="bids"),secondary_y=True,)
-                    fig.update_layout(title_text="cash_equivelant")
-                    st.plotly_chart(fig, use_container_width=True)
+                    # fig = make_subplots(specs=[[{"secondary_y": True}]])
+                    # fig.add_trace(go.Scatter(x=asks['accumulated_avg_price'], y=asks['cash_equivelant'], name="asks"),secondary_y=True,)
+                    # fig.add_trace(go.Scatter(x=bids['accumulated_avg_price'], y=bids['cash_equivelant'], name="bids"),secondary_y=True,)
+                    # fig.update_layout(title_text="cash_equivelant")
+                    # st.plotly_chart(fig, use_container_width=True)
 async def stream():
     async with websockets.connect("wss://ftx.com/ws/", ping_interval=20, ping_timeout=2000) as websocket:
         global full_dump
@@ -186,7 +188,11 @@ async def stream():
                 df1['size_new'] = df1["size"][0]
                 # df1['size_new'] = size
             
-                df = df.append(df1, ignore_index=True)
+                # df = df.append(df1, ignore_index=True)
+                # df = df.append( df1, ignore_index=True)
+
+                df = pd.concat([df, pd.DataFrame.from_records(df1)])
+
                 # df = df.append(size, ignore_index=True)
                 # st.write(size)
                 with placeholder2.container():
