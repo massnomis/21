7bf0559db2b1c4d3f741c0cc8676c99c1d864f68
vladiguez <ylannguez@gmail.com>
2022-06-28 10:28:16 +0000

    asidlksd

--- cashcarryOLHC.py
+++ cashcarryOLHC.py
@@ -643,9 +643,9 @@ for index, row in new.iterrows():
                 bbbbbbb = px.line(custom_lending,x='time',y=['Bollinger High','Bollinger Low','rate_bps_hr','rolling_mean'],render_mode="SVG")
                 st.plotly_chart(bbbbbbb, use_container_width=True)
 
-                latest_rateAPY_spot = custom['rate_APY'].iloc[-1]
+                latest_rateAPY_spot = custom_lending['rate_APY'].iloc[-1]
                 st.write("Latest Funding rate APY", latest_rateAPY)
-                latest_rate_bps_hr = custom['rate'].iloc[-1]
+                latest_rate_bps_hr = custom_lending['rate'].iloc[-1]
                 st.write("funding_rate_bps_hr", latest_rate_bps_hr)
                 return latest_rateAPY_spot, latest_rate_bps_hr, custom_lending['rateAPY'], custom_lending['rate']
         if custom_lending['success'] == 'false':
