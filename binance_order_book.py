005e194c590c73c07261178ce95b6756e4303530
massnomis <massnomis@gmail.com>
2022-07-09 06:11:41 -0400

    Update binance_order_book.py

--- binance_order_book.py
+++ binance_order_book.py
@@ -63,20 +63,20 @@ def on_message(ws, message):
 
         # asks = pd.DataFrame(asks)
         # bids = pd.DataFrame(bids)
-
-        # asks['accumulated']  = (list(accumulate(asks['size_ask'])))
-        # asks['accumulated_price']  = (asks['price_ask']) * asks['size_ask']
-        # asks['accumulated_avg_price'] = (list(accumulate(asks['accumulated_price'])))  / asks['accumulated']
-        # asks['cash_equivelant'] = asks['accumulated'] * asks['accumulated_avg_price']
-
-        # bids['accumulated']  = (list(accumulate(bids['size_bid'])))
-        # bids['accumulated_price']  = (bids['price_bid']) * bids['size_bid']
-        # bids['accumulated_avg_price'] = (list(accumulate(bids['accumulated_price'])))  / bids['accumulated']
-        # bids['cash_equivelant'] = bids['accumulated'] * bids['accumulated_avg_price']  
         bids["price_bid"] = bids["price_bid"].astype(float)
         bids["size_bid"] = bids["size_bid"].astype(float)
         asks["price_ask"] = asks["price_ask"].astype(float)
         asks["size_ask"] = asks["size_ask"].astype(float)
+        asks['accumulated']  = (list(accumulate(asks['size_ask'])))
+        asks['accumulated_price']  = (asks['price_ask']) * asks['size_ask']
+        asks['accumulated_avg_price'] = (list(accumulate(asks['accumulated_price'])))  / asks['accumulated']
+        asks['cash_equivelant'] = asks['accumulated'] * asks['accumulated_avg_price']
+
+        bids['accumulated']  = (list(accumulate(bids['size_bid'])))
+        bids['accumulated_price']  = (bids['price_bid']) * bids['size_bid']
+        bids['accumulated_avg_price'] = (list(accumulate(bids['accumulated_price'])))  / bids['accumulated']
+        bids['cash_equivelant'] = bids['accumulated'] * bids['accumulated_avg_price']  
+
         for i in range(1, 2):
             cols = st.columns(2)
             cols[0].subheader("bids")
@@ -88,23 +88,23 @@ def on_message(ws, message):
 
             cols[1].write(asks)
 
-        # fig = make_subplots(specs=[[{"secondary_y": True}]])
-        # fig.add_trace(go.Scatter(x=asks['price_ask'], y=asks['accumulated'], name="asks"),secondary_y=True,)
-        # fig.add_trace(go.Scatter(x=bids['price_bid'], y=bids['accumulated'], name="bids"),secondary_y=True,)
-        # fig.update_layout(title_text="orderbook")
-        # st.plotly_chart(fig, use_container_width=True)
-        # st.plotly_chart(px.bar(bids, x="price_bid", y="size_bid", title="bids"), use_container_width=True)
+        fig = make_subplots(specs=[[{"secondary_y": True}]])
+        fig.add_trace(go.Scatter(x=asks['price_ask'], y=asks['accumulated'], name="asks"),secondary_y=True,)
+        fig.add_trace(go.Scatter(x=bids['price_bid'], y=bids['accumulated'], name="bids"),secondary_y=True,)
+        fig.update_layout(title_text="orderbook")
+        st.plotly_chart(fig, use_container_width=True)
+        st.plotly_chart(px.bar(bids, x="price_bid", y="size_bid", title="bids"), use_container_width=True)
         fig = make_subplots(specs=[[{"secondary_y": True}]])
         fig.add_trace(go.Bar(x=bids['price_bid'], y=bids['size_bid'], name="bids"),secondary_y=True,)
         fig.add_trace(go.Bar(x=asks['price_ask'], y=asks['size_ask'], name="asks"),secondary_y=True,)
         fig.update_layout(title_text="orderbook")
         st.plotly_chart(fig, use_container_width=True)
 
-        # fig = make_subplots(specs=[[{"secondary_y": True}]])
-        # fig.add_trace(go.Scatter(x=asks['accumulated_avg_price'], y=asks['cash_equivelant'], name="asks"),secondary_y=True,)
-        # fig.add_trace(go.Scatter(x=bids['accumulated_avg_price'], y=bids['cash_equivelant'], name="bids"),secondary_y=True,)
-        # fig.update_layout(title_text="cash_equivelant")
-        # st.plotly_chart(fig, use_container_width=True)
+        fig = make_subplots(specs=[[{"secondary_y": True}]])
+        fig.add_trace(go.Scatter(x=asks['accumulated_avg_price'], y=asks['cash_equivelant'], name="asks"),secondary_y=True,)
+        fig.add_trace(go.Scatter(x=bids['accumulated_avg_price'], y=bids['cash_equivelant'], name="bids"),secondary_y=True,)
+        fig.update_layout(title_text="cash_equivelant")
+        st.plotly_chart(fig, use_container_width=True)
 
     #     column = bids["price_bid"]
     #     max_value_spot = column.max()
