31a8d5946fc4ce6521b0b656fcef18a610f55d70
massnomis <massnomis@gmail.com>
2022-07-10 08:29:06 +0000

    init huh

--- binance_trades.py
+++ binance_trades.py
@@ -27,7 +27,8 @@ def on_message(ws, message):
     with placeholder1.container():
 
         df1 = pd.DataFrame.from_dict([message])
-        
+        # df1['E'] = pd.to_datetime(df1['E'], unit='ms')
+
         df = df.append(df1, ignore_index=False)
         # m = df['m']
         df['m'] = df['m'].astype(str)
@@ -37,8 +38,8 @@ def on_message(ws, message):
         df['s'] = df['s'].astype(str)
         df['p'] = df['p'].astype(float)
         df['q'] = df['q'].astype(float)
-        # df['T'] = df['T'].astype(float)
-
+        df['T'] = df['T'].astype(str)
+        # df['sum'] = df.cumsum(['q'])
         # df['T'] = pd.to_datetime['T']
         st.write(df)
         st.plotly_chart(px.scatter(df, x="E", y="p", color="maker", size='q'),use_container_width=True)
@@ -69,3 +70,67 @@ streamTrades('btcbusd')
 # T     int64
 # m      bool
 # M      bool
+
+
+# import websocket
+# import datetime
+# import streamlit as st
+# import time
+# import json
+# import plotly.express as px
+# import pandas as pd
+# from itertools import accumulate
+# import plotly.graph_objects as go
+# from plotly.subplots import make_subplots
+# import numpy
+
+# df = pd.DataFrame()
+# fixed_df = pd.DataFrame()
+# # fixed_df = pd.DataFrame(columns=['m'], dtype=str)
+
+# st.set_page_config(layout="wide")
+# placeholder1 = st.empty()
+
+# # import websocket-client
+# def on_message(ws, message):
+#     # print()
+#     # print(str(datetime.datetime.now()) + ": ")
+#     message = json.loads(message)
+#     global df
+#     global fixed_df
+#     with placeholder1.container():
+
+#         df1 = pd.DataFrame.from_dict([message])
+        
+#         df = df.append(df1, ignore_index=False)
+#         # m = df['m']
+#         df['m'] = df['m'].astype(str)
+#         df['maker'] = df['m']
+#         df['M'] = df['M'].astype(str)
+#         df['E'] = pd.to_datetime(df['E'], unit='ms')
+#         df['s'] = df['s'].astype(str)
+#         df['p'] = df['p'].astype(float)
+#         df['q'] = df['q'].astype(float)
+#         # df['T'] = df['T'].astype(float)
+#         # E = df['E'] 
+#         # E = pd.to_datetime(E, unit='ms')
+#         # df['E'] = E
+#         # df['T'] = pd.to_datetime['T']
+#         st.write(df)
+#         st.plotly_chart(px.scatter(df, x="E", y="p", color="maker", size='q'),use_container_width=True)
+
+# def on_error(ws, error):
+#     print(error)
+
+# def on_close(close_msg):
+#     print("### closed ###" + close_msg)
+
+# def streamTrades(currency):
+#     # websocket.enableTrace(False)
+#     socket = f'wss://stream.binance.com:9443/ws/{currency}@trade'
+#     ws = websocket.WebSocketApp(socket,
+#                                 on_message=on_message,
+#                                 on_error=on_error,
+#                                 on_close=on_close)
+#     ws.run_forever()
+# streamTrades('btcbusd')
