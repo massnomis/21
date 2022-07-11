aa17fe7e77100ebe1d2ee8547842e18ce4ba5f44
massnomis <massnomis@gmail.com>
2022-07-11 19:05:03 +0000

    probably for the night

--- oder_types.py
+++ oder_types.py
@@ -35,7 +35,75 @@ bids['fixed_size_bid'] = bids['size_bid']/precision_amount
 asks = asks.drop(columns=['size_ask'])
 bids = bids.drop(columns=['size_bid'])
 
-st.write(asks, bids)
+asks = pd.DataFrame(asks)
+bids = pd.DataFrame(bids)
+# st.write(asks_update)
+# st.write(bids_update)
+asks['accumulated']  = (list(accumulate(asks['fixed_size_ask'])))
+asks['accumulated_price']  = (asks['price_ask']) * asks['fixed_size_ask']
+asks['accumulated_avg_price'] = (list(accumulate(asks['accumulated_price'])))  / asks['accumulated']
+asks['cash_equivelant'] = asks['accumulated'] * asks['accumulated_avg_price']
+
+bids['accumulated']  = (list(accumulate(bids['fixed_size_bid'])))
+bids['accumulated_price']  = (bids['price_bid']) * bids['fixed_size_bid']
+bids['accumulated_avg_price'] = (list(accumulate(bids['accumulated_price'])))  / bids['accumulated']
+bids['cash_equivelant'] = bids['accumulated'] * bids['accumulated_avg_price']                
+for i in range(1, 2):
+    cols = st.columns(2)
+    cols[0].subheader("bids")
+
+    cols[0].write(bids)
+    cols[1].subheader("asks")
+
+    cols[1].write(asks)
+# st.write(asks,bids, use_container_width=True)
+# st.write(asks_update,bids_update, use_container_width=True)
+
+fig = make_subplots(specs=[[{"secondary_y": True}]])
+fig.add_trace(go.Scatter(x=asks['price_ask'], y=asks['accumulated'], name="asks"),secondary_y=True,)
+fig.add_trace(go.Scatter(x=bids['price_bid'], y=bids['accumulated'], name="bids"),secondary_y=True,)
+fig.update_layout(title_text="orderbook")
+st.plotly_chart(fig, use_container_width=True)
+
+fig = make_subplots(specs=[[{"secondary_y": True}]])
+fig.add_trace(go.Bar(x=asks['price_ask'], y=asks['fixed_size_ask'], name="asks"),secondary_y=True,)
+fig.add_trace(go.Bar(x=bids['price_bid'], y=bids['fixed_size_bid'], name="bids"),secondary_y=True,)
+fig.update_layout(title_text="orderbook")
+st.plotly_chart(fig, use_container_width=True)
+
+fig = make_subplots(specs=[[{"secondary_y": True}]])
+fig.add_trace(go.Scatter(x=asks['accumulated_avg_price'], y=asks['cash_equivelant'], name="asks"),secondary_y=True,)
+fig.add_trace(go.Scatter(x=bids['accumulated_avg_price'], y=bids['cash_equivelant'], name="bids"),secondary_y=True,)
+fig.update_layout(title_text="cash_equivelant")
+st.plotly_chart(fig, use_container_width=True)
+
+column = bids["price_bid"]
+max_value_spot = column.max()
+st.write("now",datetime.now())
+st.write("best bid", max_value_spot)
+
+column = asks["price_ask"]
+min_value_spot = column.min()
+st.write("best ask", min_value_spot)
+
+spred_spot = min_value_spot - max_value_spot
+st.write("spot spread", spred_spot)
+
+spred_bps_spot = spred_spot/min_value_spot*1000
+st.write("spred_bps", spred_bps_spot , "bps")
+
+
+a = np.array([1, 2, 3, 4, 5])
+st.write(a)
+b = np.random.randint(10, size=(2, 10))
+st.write(b)
+arr = np.array([a, a])
+st.write(arr)
+
+
+
+
+
 for i in range(1, 2):
     colz = st.columns(2)
     # colz[0].write(pd.DataFrame(exchange.fetchCurrencies()).columns)
