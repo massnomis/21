6643dbaf43a5e36b074acce4f6f5436b3aa1ce5b
Samuel Simons <massnomis@gmail.com>
2022-08-11 22:18:58 +0300

    asd

--- newfill.ts
+++ newfill.ts
@@ -1,63 +1,12 @@
-import Web3 from 'web3';
-import {
-    LimitOrderBuilder,
-    LimitOrderProtocolFacade,
-    Web3ProviderConnector,
-} from '@1inch/limit-order-protocol';
-
-const contractAddress = '0x94Bc2a1C732BcAd7343B25af48385Fe76E08734f';
-const walletAddress = '0xdF2D2F76E8E8827f92814E49e7D95Fc1e33E4148';
-const chainId = 137;
-
-const web3 = new Web3('...');
-// You can create and use a custom provider connector (for example: ethers)
-const connector = new Web3ProviderConnector(web3);
-
-const limitOrderBuilder = new LimitOrderBuilder(
-    contractAddress,
-    chainId,
-    connector
-);
-
-const limitOrderProtocolFacade = new LimitOrderProtocolFacade(
-    contractAddress,
-    connector
-);
-
-// Create a limit order and it's signature
-const limitOrder = limitOrderBuilder.buildLimitOrder({
-    makerAssetAddress: '0xbb4cdb9cbd36b01bd1cbaebf2de08d9173bc095c',
-    takerAssetAddress: '0x111111111117dc0aa78b770fa6a738034120c302',
-    makerAddress: walletAddress,
-    makerAmount: '100',
-    takerAmount: '200',
-    predicate: '0x',
-    permit: '0x',
-    interaction: '0x',
-});
-const limitOrderTypedData = limitOrderBuilder.buildLimitOrderTypedData(
-    limitOrder
-);
-const limitOrderSignature = limitOrderBuilder.buildOrderSignature(
-    walletAddress,
-    limitOrderTypedData
-);
-
-// Create a call data for fill the limit order
-const callData = limitOrderProtocolFacade.fillLimitOrder(
-    limitOrder,
-    limitOrderSignature,
-    '100',
-    '0',
-    '50'
-);
-
-// Send transaction for the order filling
-// Must be implemented
-sendTransaction({
-    from: walletAddress,
-    gas: 210_000, // Set your gas limit
-    gasPrice: 40000, // Set your gas price
-    to: contractAddress,
-    data: callData,
-});
+// '''print the numbers 50 to 100, but only the even ones'''
+for (var i = 50; i <= 100; i++) {
+    if (i % 2 == 0) {
+        console.log(i);
+    }
+}
+// Output:
+// '''fetch and compile a list of american equity partners for tis deal and print them out'''
+var equityPartners = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+for (var i = 0; i < equityPartners.length; i++) {
+    console.log(equityPartners[i]);
+}
