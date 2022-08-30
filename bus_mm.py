92438435522d4ce50fbbadca3b6b8052bf61e5b6
massnomis <massnomis@gmail.com>
2022-08-30 19:29:54 -0400

    kjh

--- bus_mm.py
+++ bus_mm.py
@@ -1,3 +1,5 @@
+from operator import index
+from turtle import title
 import ccxtpro
 import asyncio
 from numpy import place
@@ -7,12 +9,18 @@ import pandas as pd
 from itertools import accumulate
 import plotly.graph_objects as go
 from plotly.subplots import make_subplots
+import datetime
+
 import ccxt
+import plotly.express as px
 
 st.set_page_config(layout="wide")
-placeh = st.empty()
-placeg = st.empty()
-placeholder = st.empty()
+
+
+
+
+
+placeholder0 = st.empty()
 placeholder1 = st.empty()
 placeholder2 = st.empty()
 placeholder3 = st.empty()
@@ -29,27 +37,69 @@ placeholder13 = st.empty()
 placeholder14 = st.empty()
 placeholder15 = st.empty()
 
+
 exchange = ccxtpro.binanceus({'enableRateLimit': True})
-ccxtbus = ccxt.binanceus()
+ccxtbus = ccxt.binanceus({
+    'apiKey': 'iaibitzwWrG7etIm9PFyBdfJe7JsB84UZuQIYpM5YyfIsGAEM6QD4AddJ3y584Ic',
+    'secret': 'U83fuUStTZ41JU10BXlHBWiqmMTaM4dAQVyL7jwMrSEQevLChJmJtEx3b7QxjM50',
+})
 markets = ccxtbus.load_markets()
-# markets = json.loads(markets)
 markets = pd.DataFrame.from_dict(markets)
-# markets = markets['0']
 df1 = pd.DataFrame.from_dict(markets)
 df1 = df1.astype(str)
-# df1 = pd.DataFrame.from_dict(markets)
 df1 = df1.columns
-# st.write(df1)
-with placeh.container():
+with placeholder0.container():
     market = st.selectbox("Select Market", df1)
-
-
-async def main():
-    # exchange = ccxtpro.binanceus({'enableRateLimit': True})
+with placeholder10.container():
+    precision_load = pd.DataFrame(ccxtbus.load_markets())
+    recision_load = precision_load.astype(str)
+    # st.write(recision_load)
+    min_size = (precision_load[market]['limits']['cost']['min'])
+    precision_base = (precision_load[market]['precision']['base'])
+    precision_quote = (precision_load[market]['precision']['quote'])
+    base = (precision_load[market]['base'])
+    quote = (precision_load[market]['quote'])
+    st.write(f"Precision: {base} {precision_base} {quote} {precision_quote}")
+    st.write(f"Min Size: {min_size}")
+async def OLHC():
     while True:
-       
+        ohlc = await exchange.watch_ohlcv(market)
+        ohlc = pd.DataFrame(ohlc)
+        ohlc.reset_index(drop=True, inplace=False)
+        ohlc = ohlc.rename(columns={0: "time", 1: "open", 2: "high", 3: "low", 4: "close", 5: "volume"})
+        ohlc['time'] = pd.to_datetime(ohlc['time'], unit='ms')
+        with placeholder1:
+            fig = go.Figure()
+            fig = make_subplots(rows=2, cols=1, shared_xaxes=True, 
+            vertical_spacing=0.03, subplot_titles=('OHLC', 'Volume'), 
+            row_width=[0.2, 0.7])
+            fig.add_trace(go.Candlestick(x=ohlc['time'], open=ohlc['open'], high=ohlc['high'], low=ohlc['low'], close=ohlc['close'], name = "OHLC"), row=1, col=1)
+            fig.add_trace(go.Bar(x=ohlc['time'], y=ohlc['volume'],showlegend=False, name = 'volume'), row=2, col=1)
+            fig.update(layout_xaxis_rangeslider_visible=False)
+            fig.update_layout(title_text="OHLC")
+            st.plotly_chart(fig, use_container_width=True)
 
+async def trades():
+    while True:
+        trades = await exchange.watch_trades(market)
+        tradez = pd.DataFrame.from_dict(trades)
+        with placeholder2.container():
+            st.plotly_chart(px.scatter(tradez, x="datetime", y="price", color="side", size='amount',color_discrete_sequence=["red", "green"],), use_container_width=True)
+async def books():
+    spread_bps_df_2 = pd.DataFrame()
+    # bids_h = pd.DataFrame()
+    # asks_h = pd.DataFrame()
+    while True:
         orderbook = await exchange.watch_order_book(market)
+
+
+        # orderbook = pd.DataFrame.from_dict(orderbook)
+        # orderbook = orderbook.astype(str)
+        # orderbook['time'] = datetime.datetime.now()
+        # books_heatmap = books_heatmap.append(orderbook)
+
+
+
         bids = orderbook["bids"]
         bids = pd.DataFrame(bids)
         bids = bids.rename(columns={0: "price_bid", 1: "size_bid"})
@@ -58,8 +108,6 @@ async def main():
         asks.reset_index(drop=True, inplace=False)
         bids.reset_index(drop = True, inplace=False)
         asks = asks.rename(columns={0: "price_ask", 1: "size_ask"})
-
-
         asks['accumulated']  = (list(accumulate(asks['size_ask'])))
         asks['accumulated_price']  = (asks['price_ask']) * asks['size_ask']
         asks['accumulated_avg_price'] = (list(accumulate(asks['accumulated_price'])))  / asks['accumulated']
@@ -68,37 +116,22 @@ async def main():
         bids['accumulated_price']  = (bids['price_bid']) * bids['size_bid']
         bids['accumulated_avg_price'] = (list(accumulate(bids['accumulated_price'])))  / bids['accumulated']
         bids['cash_equivelant'] = bids['accumulated'] * bids['accumulated_avg_price']       
-         
-
-
-
-        with placeholder:
-            for i in range(1, 2):
-                cols = st.columns(2)
-                cols[0].subheader("bids")
-                cols[0].write(bids)
-                cols[1].subheader("asks")
-                cols[1].write(asks)
-        with placeholder1:
+        spread_bps_df_add = pd.DataFrame({'time': [datetime.datetime.now()], 'spread_bps': [(((asks['price_ask'][0] - bids['price_bid'][0])/asks['price_ask'][0]) * 10000)]})
+        spread_bps_df_2 = spread_bps_df_2.append(spread_bps_df_add)
+        with placeholder3:
             fig = make_subplots(specs=[[{"secondary_y": True}]])
             fig.add_trace(go.Scatter(x=asks['price_ask'], y=asks['accumulated'], name="asks"),secondary_y=True,)
             fig.add_trace(go.Scatter(x=bids['price_bid'], y=bids['accumulated'], name="bids"),secondary_y=True,)
             fig.update_layout(title_text="orderbook")
             st.plotly_chart(fig, use_container_width=True)
-        with placeholder2:
-            fig = make_subplots(specs=[[{"secondary_y": True}]])
-            fig.add_trace(go.Bar(x=asks['price_ask'], y=asks['size_ask'], name="asks"),secondary_y=True,)
-            fig.add_trace(go.Bar(x=bids['price_bid'], y=bids['size_bid'], name="bids"),secondary_y=True,)
-            fig.update_layout(title_text="orderbook")
-            st.plotly_chart(fig, use_container_width=True)
-        with placeholder3:
-            fig = make_subplots(specs=[[{"secondary_y": True}]])
-            fig.add_trace(go.Scatter(x=asks['accumulated_avg_price'], y=asks['cash_equivelant'], name="asks"),secondary_y=True,)
-            fig.add_trace(go.Scatter(x=bids['accumulated_avg_price'], y=bids['cash_equivelant'], name="bids"),secondary_y=True,)
-            fig.update_layout(title_text="cash_equivelant")
-            st.plotly_chart(fig, use_container_width=True)
+        with placeholder4:
+            st.plotly_chart(px.scatter(spread_bps_df_2, x="time", y="spread_bps"), use_container_width=True)
+async def main():
+    # Schedule three calls *concurrently*:
+        await asyncio.gather(
+        trades(),
+        books(),
+        OLHC(),
+    )
 
-loop = asyncio.new_event_loop()
-asyncio.set_event_loop(loop)
-while True:
-    loop.run_until_complete(main())
+asyncio.run(main())   
